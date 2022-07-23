537eebad97c7fb492e2edff561f0e4605ed13367
akrawczyk <akrawczyk@cad-schroer.de>
2022-07-23 23:43:42 +0200

    project files

---
+++ PseudoOrk/Commands/Commands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PseudoOrk.Commands
+{
+    internal class Command
+    {
+        private string input;
+        private FileSystem fileSystem = new FileSystem();
+
+        public void ReadLine()
+        {
+            Console.Write("<" + fileSystem.GetPath() + "> : ");
+            input = Console.ReadLine();
+        }
+        public void HandleLine()
+        {
+            try
+            {
+                fileSystem.EnterPath(Type.GetType("PseudoOrk.Commands." + input, true, true));
+                Console.WriteLine("<"+ fileSystem.GetPath() + "> :");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(input+ " Could not be Found");
+            }
+        }
+    }
+}
